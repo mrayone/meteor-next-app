d15368c265ce57f3bd2083bdc98a7961a5c05102
Maycon Rayone <maycon.rayone@gmail.com>
2020-10-06 23:11:52 -0300

    feat: requisição de produtos recomendados. # função para renderização de dados client-side e server-side. Server-side para indexação em motores de busca. Cliente-side para fornecer uma melhor experiência ao usuário.

---
+++ Readme.md
@@ -0,0 +1,19 @@
+## Aprendendo sobre NextJS
+
+O NextJS é um framework que utiliza a lib react para desenvolvimento de aplicações web. O seu maior foco é o ambiente de produção, pois o mesmo possui uma vasta gama de configurações e ajustes in-box para facilitar a nossa vida no desenvolvimento de SPAS e tratamento de SSR (Server-side Rendering).
+
+### 1. Como estrutura de pastas e organização.
+
+#### 1.2 Suporte ao TypeScript.
+
+### 2. Rotas.
+
+#### 2.1 Rotas Dinâmicas.
+
+### 3. Styled components e contexto de renderização SSR.
+
+### 4. Client-side fetching
+
+Chamadas a api realizadas pelo browser, normalmente são informações que não existem a necessidade de serem indexadas pelos motores de busca. Normalmente utilizaremos o fetchApi, axios ou SWR da própria vercel.
+
+### 5. Server-side fetching
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,51 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import { Title } from "../styles/pages/Home";
-export default function Home() {
+
+interface IProduct {
+  id: number;
+  title: String;
+}
+
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({ recommendedProducts }: HomeProps) {
+  // const [recommendedProducts, setRecommendedProducts] = useState<
+  //   Array<IProduct>
+  // >([]);
+
+  // useEffect(() => {
+  //   fetch("http://localhost:3333/recommended").then((response) => {
+  //     response.json().then((data) => {
+  //       setRecommendedProducts(data);
+  //     });
+  //   });
+  // }, []);
+
   return (
     <div>
-      <Title>Hello world!</Title>
+      <section>
+        <Title>Recommended Products</Title>
+
+        <ul>
+          {recommendedProducts.map((product) => (
+            <li key={product.id}>{product.title}</li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch("http://localhost:3333/recommended");
+  const recommendedProducts = await response.json();
+  return {
+    props: {
+      recommendedProducts,
+    },
+  };
+};
